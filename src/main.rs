073ead3c596090d6f6b8e5630ef70c07300ee015
philsippl <philsippl@gmail.com>
2024-04-22 21:10:55 +0000

    up

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-// node 1: FI_EFA_USE_DEVICE_RDMA=1 NCCL_NET=socket ./target/release/gpu-bench 0 0 2
-// node 2: FI_EFA_USE_DEVICE_RDMA=1 NCCL_NET=socket NCCL_SOCKET_IFNAME=ens32 NCCL_DEBUG=INFO ./target/release/gpu-bench 1 0 2 ea4ce654861caacd0200c787ac1f29530000000000000000000000000000000000000000000000001019794c85550000b0e20ce2ff7f00000050b2c8b87f0000b0e00ce2ff7f000082f38fc8b87f00000100000000000000100000003000000080e20ce2ff7f0000b0e10ce2ff7f00000180adfb855500002919794c85550000
-
 use std::{
-    env, str::FromStr, sync::Arc, time::{Duration, Instant}
+    env, str::FromStr, sync::Arc, time::Instant
 };
 
 use atomic_float::AtomicF64;
@@ -14,8 +11,8 @@ use cudarc::{
     },
 };
 use once_cell::sync::Lazy;
-use tokio::{sync::{Mutex, MutexGuard}, task::JoinHandle, time::{self, sleep}};
-use std::sync::atomic::Ordering::{Acquire, Release};
+use tokio::task::JoinHandle;
+use std::sync::atomic::Ordering::{Acquire, SeqCst};
 
 static COMM_ID: Lazy<Vec<Id>> = Lazy::new(|| {
     (0..CudaDevice::count().unwrap())
@@ -118,7 +115,7 @@ async fn main() -> eyre::Result<()> {
                     throughput * 8f64
                 );
 
-                total_throughput_clone.fetch_add(throughput * 8f64, std::sync::atomic::Ordering::SeqCst);
+                total_throughput_clone.fetch_add(throughput * 8f64, SeqCst);
             }
         });
         handles.push(handle);
@@ -128,69 +125,7 @@ async fn main() -> eyre::Result<()> {
         handle.await?;
     }
 
-    println!("Total throughput: {:?} Gbps", total_throughput);
-
-    // time::sleep(Duration::from_secs(100)).await;
+    println!("Total throughput: {:.2} Gbps", total_throughput.load(Acquire));
 
     Ok(())
 }
-
-// fn main() {
-//     // NCCL_COMM_ID
-//     // let n_devices = CudaDevice::count().unwrap() as usize;
-
-//     let args = env::args().collect::<Vec<_>>();
-//     let rank = args[1].parse().unwrap();
-//     let device_id = args[2].parse().unwrap();
-//     let n_devices = args[3].parse().unwrap();
-
-//     println!("000000");
-
-//     let id = if rank == 0 {
-//         let id = Id::new().unwrap();
-//         println!("{:?}", IdWrapper(id.clone()).to_string());
-//         id
-//     } else {
-//         let id = IdWrapper::from_str(&args[4]).unwrap().0;
-//         println!("{:?}", IdWrapper(id.clone()).to_string());
-//         id
-//     };
-
-//     println!("1111");
-
-//     // hex::encode(id.internal());
-//     let dev = CudaDevice::new(device_id).unwrap();
-
-//     let mut slice: CudaSlice<u8> = dev.alloc_zeros(LEN).unwrap();
-
-//     let comm = Comm::from_rank(dev.clone(), rank, n_devices, id).unwrap();
-
-//     println!("222");
-
-//     // let slice = dev.htod_copy(vec![1337 as i32]).unwrap();
-
-//     let peer: i32 = (rank as i32 + 1) % 2;
-
-//     for i in 0..10 {
-//         if rank == 0 {
-//             println!("sending from {} to {}: {:?}", rank, peer, slice);
-//             comm.send(&slice, peer).unwrap();
-//             println!("sent from {} to {}: {:?}", rank, peer, slice);
-//         } else {
-//             println!("waiting for msg from peer {} ...", peer);
-//             let now = Instant::now();
-//             comm.recv(&mut slice, peer).unwrap();
-//             dev.synchronize().unwrap();
-//             let elapsed = now.elapsed();
-//             let throughput =(LEN as f64) / (elapsed.as_millis() as f64) / 1_000_000_000f64 * 1_000f64;
-//             println!(
-//                 "received in {:?} [{:.2} GB/s] [{:.2} Gbps]",
-//                 elapsed,
-//                 throughput,
-//                 throughput * 8f64
-//             );
-//         }
-//     }
-
-//     std::thread::sleep(std::time::Duration::from_secs(30));
-// }
