96a83783a61f698a74a59f7b66c0e8aec44ab0a0
philsippl <philsippl@gmail.com>
2024-04-22 19:49:45 +0000

    up

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 // node 1: FI_EFA_USE_DEVICE_RDMA=1 NCCL_NET=socket ./target/release/gpu-bench 0 0 2
 // node 2: FI_EFA_USE_DEVICE_RDMA=1 NCCL_NET=socket NCCL_SOCKET_IFNAME=ens32 NCCL_DEBUG=INFO ./target/release/gpu-bench 1 0 2 ea4ce654861caacd0200c787ac1f29530000000000000000000000000000000000000000000000001019794c85550000b0e20ce2ff7f00000050b2c8b87f0000b0e00ce2ff7f000082f38fc8b87f00000100000000000000100000003000000080e20ce2ff7f0000b0e10ce2ff7f00000180adfb855500002919794c85550000
 
-use std::{env, str::FromStr, time::{Duration, Instant}};
+use std::{
+    env,
+    str::FromStr,
+    time::{Duration, Instant},
+};
 
-use axum::{extract::Path, http::StatusCode, routing::get, Json, Router};
+use axum::{extract::Path, routing::get, Router};
 use cudarc::{
     driver::{CudaDevice, CudaSlice},
     nccl::{
-        group_end, group_start,
-        result::{all_reduce, comm_init_rank, get_uniqueid},
-        Comm, Id, ReduceOp,
+        Comm, Id,
     },
 };
 use once_cell::sync::Lazy;
 use tokio::time::sleep;
 
-static COMM_ID: Lazy<Id> = Lazy::new(|| Id::new().unwrap());
+static COMM_ID: Lazy<Vec<Id>> = Lazy::new(|| {
+    (0..CudaDevice::count().unwrap())
+        .map(|_| Id::new().unwrap())
+        .collect::<Vec<_>>()
+});
 
 struct IdWrapper(Id);
 
@@ -50,9 +56,9 @@ impl ToString for IdWrapper {
 
 const DUMMY_DATA_LEN: usize = 35 * (1 << 30);
 
-// Path(device_id): Path<String>
-async fn root() -> String {
-    IdWrapper(*COMM_ID).to_string()
+async fn root(Path(device_id): Path<String>) -> String {
+    let device_id: usize = device_id.parse().unwrap();
+    IdWrapper(COMM_ID[device_id]).to_string()
 }
 
 #[tokio::main]
@@ -62,35 +68,43 @@ async fn main() -> eyre::Result<()> {
     let n_devices = CudaDevice::count().unwrap() as usize;
     let party_id: usize = args[1].parse().unwrap();
 
-    let id = if party_id == 0 {
+    if party_id == 0 {
         tokio::spawn(async move {
             let app = Router::new().route("/", get(root));
 
             let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
             axum::serve(listener, app).await.unwrap();
         });
-        *COMM_ID
-    } else {
-        let res = reqwest::get(format!("http://{}", args[2])).await?;
-        IdWrapper::from_str(&res.text().await?).unwrap().0
     };
 
     for i in 0..n_devices {
         tokio::spawn(async move {
+            let args = env::args().collect::<Vec<_>>();
+
+            let id = if party_id == 0 {
+                COMM_ID[i]
+            } else {
+                let res = reqwest::get(format!("http://{}/{}", args[2], i)).await.unwrap();
+                IdWrapper::from_str(&res.text().await.unwrap()).unwrap().0
+            };
+
             let dev = CudaDevice::new(i).unwrap();
             let mut slice: CudaSlice<u8> = dev.alloc_zeros(DUMMY_DATA_LEN).unwrap();
             let comm = Comm::from_rank(dev.clone(), party_id + i, n_devices, id).unwrap();
 
             let peer_party: i32 = (party_id as i32 + 1) % 2;
+            let peer_device = peer_party * n_devices as i32 + i as i32;
 
             if party_id == 0 {
-                comm.send(&slice, peer_party + i as i32).unwrap();
+                println!("sending from {} to {}....", party_id + i, peer_device);
+                comm.send(&slice, peer_device).unwrap();
             } else {
                 let now = Instant::now();
-                comm.recv(&mut slice, peer_party).unwrap();
+                comm.recv(&mut slice, peer_device).unwrap();
                 let elapsed = now.elapsed();
                 let throughput =
-                    (DUMMY_DATA_LEN as f64) / (elapsed.as_millis() as f64) / 1_000_000_000f64 * 1_000f64;
+                    (DUMMY_DATA_LEN as f64) / (elapsed.as_millis() as f64) / 1_000_000_000f64
+                        * 1_000f64;
                 println!(
                     "received in {:?} [{:.2} GB/s] [{:.2} Gbps]",
                     elapsed,
