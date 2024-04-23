af41d8799144bfb6144920c99f88147c87559871
philsippl <philsippl@gmail.com>
2024-04-23 14:23:02 +0000

    up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{env, str::FromStr, sync::{Arc, Barrier}, time::Instant};
+use std::{
+    env,
+    str::FromStr,
+    sync::{Arc, Barrier},
+    thread::{self, JoinHandle},
+    time::Instant,
+};
 
 use atomic_float::AtomicF64;
 use axum::{extract::Path, routing::get, Router};
@@ -8,9 +14,6 @@ use cudarc::{
 };
 use once_cell::sync::Lazy;
 use std::sync::atomic::Ordering::{Acquire, SeqCst};
-use tokio::{
-    task::{JoinHandle, LocalSet},
-};
 
 static COMM_ID: Lazy<Vec<Id>> = Lazy::new(|| {
     (0..CudaDevice::count().unwrap())
@@ -62,10 +65,9 @@ async fn main() -> eyre::Result<()> {
     let n_devices = CudaDevice::count().unwrap() as usize;
     let party_id: usize = args[1].parse().unwrap();
     let total_throughput = Arc::new(AtomicF64::new(0.0));
-    let local1 = LocalSet::new();
-    
+
     if party_id == 0 {
-        local1.spawn_local(async move {
+        tokio::spawn(async move {
             println!("starting server...");
             let app = Router::new().route("/:device_id", get(root));
             let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -75,12 +77,11 @@ async fn main() -> eyre::Result<()> {
 
     let barrier = Arc::new(Barrier::new(n_devices));
     let mut handles: Vec<JoinHandle<()>> = vec![];
-    let local = LocalSet::new();
 
     for i in 0..n_devices {
         let total_throughput_clone = Arc::clone(&total_throughput);
         let c = barrier.clone();
-        let handle = local.spawn_local(async move {
+        let handle = thread::spawn(move || {
             let args = env::args().collect::<Vec<_>>();
 
             let id = if party_id == 0 {
@@ -95,56 +96,52 @@ async fn main() -> eyre::Result<()> {
 
             println!("starting device {i}...");
 
-            tokio::task::spawn_blocking(move ||{
-                let comm = Comm::from_rank(dev.clone(), party_id, 2, id).unwrap();
+            let comm = Comm::from_rank(dev.clone(), party_id, 2, id).unwrap();
 
-                c.wait();
-    
-                let peer_party: i32 = (party_id as i32 + 1) % 2;
-    
-                if party_id == 0 {
-                    println!("sending from {} to {} (device {})....", party_id, peer_party, i);
-                    comm.send(&slice, peer_party).unwrap();
-                    dev.synchronize().unwrap();
-                } else {
-                    let now = Instant::now();
-                    comm.recv(&mut slice, peer_party).unwrap();
-                    dev.synchronize().unwrap();
-                    let mut xx = [0u8;1];
-                    dev.dtoh_sync_copy_into(&slice, &mut xx);
-                    let elapsed = now.elapsed();
-                    let throughput =
-                        (DUMMY_DATA_LEN as f64) / (elapsed.as_millis() as f64) / 1_000_000_000f64
-                            * 1_000f64;
-                    println!(
-                        "received in {:?} [{:.2} GB/s] [{:.2} Gbps]",
-                        elapsed,
-                        throughput,
-                        throughput * 8f64
-                    );
-    
-                    total_throughput_clone.fetch_add(throughput * 8f64, SeqCst);
-                }
-            });
+            c.wait();
 
-           
+            let peer_party: i32 = (party_id as i32 + 1) % 2;
+
+            if party_id == 0 {
+                println!(
+                    "sending from {} to {} (device {})....",
+                    party_id, peer_party, i
+                );
+                comm.send(&slice, peer_party).unwrap();
+                dev.synchronize().unwrap();
+            } else {
+                let now = Instant::now();
+                comm.recv(&mut slice, peer_party).unwrap();
+                dev.synchronize().unwrap();
+                let elapsed = now.elapsed();
+                let throughput =
+                    (DUMMY_DATA_LEN as f64) / (elapsed.as_millis() as f64) / 1_000_000_000f64
+                        * 1_000f64;
+                println!(
+                    "received in {:?} [{:.2} GB/s] [{:.2} Gbps]",
+                    elapsed,
+                    throughput,
+                    throughput * 8f64
+                );
+
+                total_throughput_clone.fetch_add(throughput * 8f64, SeqCst);
+            }
         });
+
         handles.push(handle);
     }
 
-    local.await;
+    // local.await;
     // local1.await;
 
-    // for handle in handles {
-    //     handle.await?;
-    // }
+    for handle in handles {
+        handle.join();
+    }
 
     println!(
         "Total throughput: {:.2} Gbps",
         total_throughput.load(Acquire)
     );
-
-    local1.await;
 
     Ok(())
 }
